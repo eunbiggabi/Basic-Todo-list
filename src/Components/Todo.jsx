4c19adae5f8451c58cd8cafb883d727fe679e357
eunbiggabi <eunbiggabi84@gmail.com>
2024-07-09 01:13:43 +1000

    added saving function on local storage

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useRef, useState } from 'react'
 import './CSS/Todo.css'
+import Todoitems from './Todoitems';
 
+let count = 0;
 const Todo = () => {
+
+ const [todos, setTodos] = useState([]);
+ const inputRef = useRef(null);
+
+ const add = () => {
+    setTodos([...todos, {id: count++, text: inputRef.current.value, display:""}]);
+    inputRef.current.value = "";
+    localStorage.setItem('todos_count', count);
+ }
+
+ useEffect(() => {
+    setTodos(JSON.parse(localStorage.getItem("todos")))
+    count = localStorage.getItem("todos_count");
+},[])
+
+ useEffect(() => {
+     setTimeout(() => {
+        console.log(todos);
+        localStorage.setItem("todos", JSON.stringify(todos))
+    }, 100)
+ }, [todos]);
+
   return (
     <div className="todo">
         <div className="todo-header">To-Do List</div>
         <div className="todo-add">
-            <input type="text" placeholder="Add Your Task" className="todo-input"/>
-            <div className="todo-add-btn">ADD</div>
+            <input ref={inputRef} type="text" placeholder="Add Your Task" className="todo-input"/>
+            <div onClick={() => add()} className="todo-add-btn">ADD</div>
         </div>
-
+        {todos.map((todo, index) => (
+            <Todoitems key={index}  id={todo.id} text={todo.text} display={todo.display} />
+        ))}
     </div>
   )
 }
--- src/Components/Todoitems.jsx
+++ src/Components/Todoitems.jsx
@@ -1,8 +1,18 @@
 import './CSS/Todoitems.css'
+import tick from './Assets/tick.png'
+import not_tick from './Assets/not_tick.png'
+import cross from './Assets/cross.png'
 
-const Todoitems = () => {
+const Todoitems = ({id, text, display}) => {
   return (
-    <div>Todoitems</div>
+    <div className='todoitems'>
+        <div className='todoitems-container'>
+            <img src={not_tick} alt="" />
+            <img src={tick} alt="" />
+            <div className="todoitems-text">{text}</div>
+        </div>
+        <img className='todoitems-cross-icon' src={cross} alt="" />
+    </div>
   )
 }
 
