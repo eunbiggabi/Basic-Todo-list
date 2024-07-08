6ef0d117ab081b97634928a628f0cc63afca0d2a
eunbiggabi <eunbiggabi84@gmail.com>
2024-07-08 17:08:39 +1000

    setup HTML and CSS

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./index.css"
+import Todo from "./Components/Todo"
+
+
+
+function App() {
+
+  return (
+    <div>
+      <Todo />
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/Components/Todoitems.jsx
@@ -0,0 +1,9 @@
+import './CSS/Todoitems.css'
+
+const Todoitems = () => {
+  return (
+    <div>Todoitems</div>
+  )
+}
+
+export default Todoitems
